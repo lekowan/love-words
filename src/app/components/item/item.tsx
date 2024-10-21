1cad43b2376089ecab586e850d7f0d0e7f37cbc2
lekowan <antoine.pommert@gmail.com>
2024-10-21 23:29:45 +0100

    add content and styling

--- src/app/components/item/item.tsx
+++ src/app/components/item/item.tsx
@@ -5,16 +5,20 @@ interface ItemProps {
 }
 
 export const Item = ({ title, maxNumberOfLines, progress }: ItemProps) => {
+  const progression =
+    progress === maxNumberOfLines
+      ? "Finished"
+      : `${progress}  / ${maxNumberOfLines}`
   return (
     <>
       <div className="tw-flex tw-flex-nowrap tw-p-4 tw-items-center">
-        <div className="thumbnail tw-w-14 tw-h-14"></div>
-        <div className="tw-p-4">{title}</div>
-        {progress && maxNumberOfLines && (
-          <div>
-            {progress} / {maxNumberOfLines}
-          </div>
-        )}
+        <div className="thumbnail tw-w-14 tw-h-14 tw-shrink-0"></div>
+        <div className="tw-flex tw-flex-col tw-pl-4">
+          <div className="tw-text">{title}</div>
+          {progress && maxNumberOfLines && (
+            <div className="tw-text-sm">{progression}</div>
+          )}
+        </div>
       </div>
     </>
   )
